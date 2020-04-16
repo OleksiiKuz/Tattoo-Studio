489a92e5a03c0451355ee4d617abb61b6559a3fa
OleksiiKuz <61288803+OleksiiKuz@users.noreply.github.com>
2020-04-16 22:56:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
   });
 });
 
+
+
+
 $('.header__burger').on('click', function (e) {
   e.preventDefault;
   $(this).toggleClass('header__burger-active')
@@ -16,7 +19,56 @@ $('.header__burger').on('click', function (e) {
 
 });
 
+$('.button--promo').on('click', function (e) {
+  event.preventDefault();
+  $('.modal-main').toggleClass('modal-active')
+
+});
+
+$('.button--masters-1').on('click', function (e) {
+  event.preventDefault();
+  $('.modal--1').toggleClass('modal-active')
+
+});
+
+$('.button--masters-2').on('click', function (e) {
+  event.preventDefault();
+  $('.modal--2').toggleClass('modal-active')
+
+});
+
+$('.button--masters-3').on('click', function (e) {
+  event.preventDefault();
+  $('.modal--3').toggleClass('modal-active')
+
+});
 
 
+$('.modal__dialog-close').on('click', function (e) {
+  event.preventDefault();
+  $('.modal').removeClass('modal-active')
+
+});
+
+
+
+// $(document).ready(function () {
+//   $('a[href^="#"}').click(function () {
+//     event.preventDefault();
+//     var target = $(this).attr('href');
+//     $('html', 'body').animate({
+//       scrollTop: $(target).offset().top
+//     }, 500);
+//   });
+// });
+
+
+$('a[href*="#"]').on('click', function (e) {
+  e.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 1000, 'linear');
+});
 
 
